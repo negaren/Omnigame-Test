b68a33138765ab06b4d5749a00704ee0eac73b4b
Negar Eghbali <negar.en@gmail.com>
2022-10-17 17:45:28 +0200

    draft code of dark-mode feature wirh presist user setting

---
+++ dark-mode/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+  const storedDarkMode = localStorage.getItem("DARK_MODE");
+
+  const [darkMode, setDarkMode] = useState(storedDarkMode);
+
+  console.log(darkMode);
+
+  const toggleDarkMode = () => {
+    setDarkMode(darkMode ? false : true)
+  };
+
+  useEffect(() => {
+    localStorage.setItem("DARK_MODE", darkMode);
+    console.log(localStorage.getItem("DARK_MODE"));
+  }, [darkMode]);
+
+  return (
+    <div className="App" data-theme={darkMode ? "dark" : "light"}>
+      <h1>Hello there!</h1>
+      <h2>
+        Let's create a dark mode toggle that persists when the page is reloaded.
+      </h2>
+      <button onClick={toggleDarkMode}>{darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}</button>
+    </div>
+  );
+}
+
+export default App;
